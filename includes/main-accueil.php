c153d0d731305c87494ed296a83aa2b4d0941be4
Christophe Millot <cmillot2004@gmail.com>
2026-01-05 20:37:42 +0100

    Version stable : organisation des compétences par familles et correction des boutons

--- includes/main-accueil.php
+++ includes/main-accueil.php
@@ -10,59 +10,60 @@
 </section>
 
 <section id="intro" class="intro-globale">
+
     <div class="container">
-        <h1>Bienvenue sur mon nouveau projet 2026</h1>
-        <p class="lead">Développeur & Designer Graphique. Explorez mes univers créatifs à travers ce portfolio.</p>
-        
-        <div class="introduction">
-            <h2>Titre secondaire : Introduction</h2>
-            <span class="blocs-introduction">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus repellat aperiam fugit aspernatur consequuntur veniam. Eveniet cum modi necessitatibus labore, odit tenetur vero eligendi est ex quas dignissimos animi deleniti?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus repellat aperiam fugit aspernatur consequuntur veniam. Eveniet cum modi necessitatibus labore, odit tenetur vero eligendi est ex quas dignissimos animi deleniti?</p>
-            </span>
-        </div>  
+
+        <div class="etat-civil">
+            <h1>Christophe MILLOT</h1>
+            <p class="lead"><b>Développeur & Designer Graphique. Explorez mes univers créatifs à travers ce portfolio.</b></p>
+            
+            <div class="introduction"> <h2>Welcome on board !</h2>
+                <div class="blocs-introduction">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus repellat aperiam fugit aspernatur consequuntur veniam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus repellat aperiam fugit aspernatur consequuntur veniam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus repellat aperiam fugit aspernatur consequuntur veniam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus repellat aperiam fugit aspernatur consequuntur veniam.</p>
+                </div>
+            </div>
+        </div>
+
     </div>
+
 </section>
 
 <section id="technos" class="technos-section">
     <div class="container">
-        <h2>Expertise Graphique & Technique</h2>
-        <div class="tech-grid">
+        <h2>Savoir-faire Technique & Créatif</h2> <div class="tech-grid">
             
-            <div class="tech-item">
-                <i class="fab fa-html5" aria-hidden="true"></i>
-                <span>HTML5 / CSS3</span>
-            </div>
-
-            <div class="tech-item">
-                <i class="fab fa-sass" aria-hidden="true"></i>
-                <span>Sass / SCSS</span>
-            </div>
-
-            <div class="tech-item">
-                <i class="fab fa-figma" aria-hidden="true"></i>
-                <span>UX-UI Design (Figma)</span>
-            </div>
-
-            <div class="tech-item">
+            <button class="tech-item">
                 <i class="fas fa-pen-nib" aria-hidden="true"></i>
                 <span>Infographie (Adobe)</span>
-            </div>
+            </button>
+            <button class="tech-item">
+                <i class="fab fa-figma" aria-hidden="true"></i>
+                <span>UX-UI Design (Figma)</span>
+            </button>
 
-            <div class="tech-item">
+            <button class="tech-item">
+                <i class="fab fa-html5" aria-hidden="true"></i>
+                <span>HTML5 / CSS3</span>
+            </button>
+            <button class="tech-item">
+                <i class="fab fa-sass" aria-hidden="true"></i>
+                <span>Sass / SCSS</span>
+            </button>
+
+            <button class="tech-item">
                 <i class="fab fa-git-alt" aria-hidden="true"></i>
                 <span>Gestion de projet (Git)</span>
-            </div>
-
-            <div class="tech-item">
+            </button>
+            <button class="tech-item">
                 <i class="fas fa-universal-access" aria-hidden="true"></i>
                 <span>Accessibilité & Ergonomie</span>
-            </div>
+            </button>
 
         </div>
     </div>
 </section>
-
+<!--
 <section class="services-grid">
     <div class="container">
         <div class="deux-blocs">
@@ -82,4 +83,4 @@
             </div>
         </div>   
         </div>
-</section>
+</section>-->
---
+++ structure.md
@@ -0,0 +1,22 @@
+/cv-christophe-millot-2026
+├── assets/
+│   ├── css/          (Laisser vide, Sass créera style.css ici)
+│   ├── img/          (Mets tes photos ici)
+│   └── js/           (Ton fichier script.js)
+├── includes/         (Tes morceaux de PHP)
+│   ├── header.php    (La navbar uniquement)
+│   ├── hero.php      (NOUVEAU : Le Hero isolé)
+│   ├── technos.php   (La grille des technos)
+│   └── footer.php    (Le bas de page)
+├── src/
+│   └── scss/
+│       ├── base/
+│       │   ├── _reset.scss
+│       │   └── _fonts.scss
+│       ├── layout/
+│       │   ├── _header.scss
+│       │   ├── _hero.scss     (Le SCSS du Hero isolé)
+│       │   ├── _technos.scss
+│       │   └── _footer.scss
+│       └── style.scss         (Le fichier maître)
+└── index.php                  (Ton point d'entrée)
